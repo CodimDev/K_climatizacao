23d55b9dd50fbf5854841fb23927ef1614038a53
lino167 <zaca793@gmail.com>
2025-12-06 16:44:20 -0300

    chore: adiciona scaffold inicial (Vite React TS) e .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#0ea5e9',
+          dark: '#0284c7'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
