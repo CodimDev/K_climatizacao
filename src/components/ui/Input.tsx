14ee36eb0514451cb4fddfa6f844d28c5614eedb
CodimDev <zaca793@gmail.com>
2025-12-07 03:17:51 -0300

    fix: corrigir casing e paths de UI\n\n- padronizar Button/Input (maiusc.)\n- ajustar tsconfig paths para UI\n- corrigir imports nos testes\n- garantir typecheck/lint/test ok

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   function Input({ className = '', ...props }, ref) {
--- src/components/ui/__tests__/Button.test.tsx
+++ src/components/ui/__tests__/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import { Button } from '../button'
+import { Button } from '../Button'
 
 describe('Button', () => {
   it('renderiza com texto e classe base', () => {
